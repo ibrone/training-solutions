099c259c9569433c25a19f10561006399ac3e0e7
Norbi <ibrone@gmail.com>
2020-11-06 15:29:55 +0100

    statement

---
+++ src/main/java/statenents/StatementMain.java
@@ -0,0 +1,20 @@
+package statenents;
+
+public class StatementMain {
+    public static void main(String[] args) {
+
+        int x = 5 + 6;
+        int y = 11 - x;
+        int z = 8;
+        boolean b = x > y;
+        boolean c = b == true || z > 5;
+
+        System.out.println(b);
+        System.out.println(c);
+
+        z = z+1;
+        System.out.println(z);
+    }
+
+
+}
