75bcfd81b308128e9fb8a50c0e6a365b8c224972
Norbi <ibrone@gmail.com>
2020-11-15 15:05:55 +0100

    tomb

---
+++ src/main/java/arrays/ArraysMain.java
@@ -0,0 +1,28 @@
+package arrays;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraysMain {
+
+    public String numberOfDaysAsString() {
+        int[] numberOfDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        return Arrays.toString(numberOfDays);
+    }
+
+    public List<String> daysofWeek(){
+        return Arrays.asList("Hétfő","Kedd","Szerda","Csütörtök","Péntek","Szombat","Vasárnap");
+    }
+}
+
+    public String multiplicationTableAsString(int size) {
+        int[][] multiplicationTable = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                multiplicationTable[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return Arrays.deepToString(multiplicationTable);
+    }
+}
