540a5a5248369730bfa3c48306276ed9710f566d
Norbi <ibrone@gmail.com>
2020-12-22 15:32:34 +0100

    lemaradás

---
+++ src/main/java/operators/OperatorsMain.java
@@ -0,0 +1,22 @@
+package operators;
+
+public class OperatorsMain {
+
+    public static void main(String[] args) {
+        Operators operators = new Operators();
+        System.out.println(operators.isEven(1));
+        System.out.println(operators.isEven(2));
+
+        System.out.println(Integer.toBinaryString(100));
+
+        System.out.println(16 >> 1);
+        System.out.println(16 << 1);
+        System.out.println(13 >> 1);
+        System.out.println(13 << 1);
+
+        System.out.println(operators.multiplyByPowerOfTwo(3, 5));
+        System.out.println(0333);
+        operators.convert();
+        operators.cannotConvert();
+    }
+}
