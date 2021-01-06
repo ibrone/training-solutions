12103f6d4c9d1d67ccc7292cd11b1d6d70dcd2d8
Norbi <ibrone@gmail.com>
2021-01-06 19:34:23 +0100

    exam02

---
+++ src/main/java/exam02/ArraySelector.java
@@ -0,0 +1,11 @@
+package exam02;
+
+
+public class ArraySelector {
+    public String selectEvens(int[] ints) {
+
+       for  (int i = 0 ; i<ints.length;i+=2){
+
+
+    }
+}
---
+++ src/main/java/exam02/cv/Skill.java
@@ -0,0 +1,34 @@
+package exam02.cv;
+
+public class Skill {
+
+    private String skill;
+    private int level;
+
+
+    public void Skill (String skill, int level) {
+        this.skill = skill;
+        this.level = level;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+
+    public String getName() {
+
+    }
+}
