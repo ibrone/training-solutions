79ef7e2d8faa6878a921d2acefccca62633d18a3
Norbi <ibrone@gmail.com>
2021-02-12 18:39:59 +0100

    elmaradasok

---
+++ src/test/java/week07d04/LabTest.java
@@ -0,0 +1,33 @@
+package week07d04;
+
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LabTest {
+
+    @Test
+    public void createWithTitle(){
+        Lab lab = new Lab ("Task");
+        assertEquals("Taksk1", lab.getTitle());
+        assertFalse(lab.isCompleted());
+    }
+
+    @Test
+    public void createWithTitleAndDate (){
+        Lab lab = new Lab("Task1", LocalDateTime.of(2020,12,9,10,0));
+        assertEquals("Task1", lab.getTitle());
+        assertTrue(lab.isCompleted());
+        assertEquals(LocalDateTime.of(2020, 12,9,10,0) lab.getCompletedAt());
+    }
+
+    @Test
+    public void completeNowTest(){
+        Lab lab = new lab ("Task1");
+        lab.complete();
+        assertTrue(lab.isCompleted());
+        assertEquals(LocalDateTime.now(), lab.getCompletedAt());
+    }
+}
